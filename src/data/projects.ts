a4adc6086073ef36c77822e79704a3f06cccf6b6
fecosta-fatec <andre.costa52@fatec.sp.gov.br>
2025-08-18 17:38:11 -0300

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,7 @@ export const projects = [
     id: 1,
     title: "Sistema de Gestão de Serviços de TI",
     technologies: "Python • HTML 5/CSS 3 • MySql",
-    description: "Uma plataforma que facilita a abertura de chamados pelos usuários e o atendimento rápido e organizado pelos técnicos.",
+    description: "Projeto desenvolvido na API do 1º semestre de DSM. Uma plataforma que facilita a abertura de chamados pelos usuários e o atendimento rápido e organizado pelos técnicos.",
     imageUrl: "img/sistemaGestaoTi.png",
     link: "https://github.com/MidNight-Tecnology/projeto-integrador-2022-1---FATEC-DSM"
   },
@@ -11,7 +11,7 @@ export const projects = [
     id: 2,
     title: "Sistema de Gestão Financeira e Comercial",
     technologies: "React • Java • MySql",
-    description: "Plataforma desenvolvida para a Pro4Tech, que otimiza o cadastro de títulos e endereços, além de automatizar relatórios de clientes adimplentes e inadimplentes por datas de pagamento, crédito e vencimento.",
+    description: "Projeto desenvolvido na API do 3º semestre de DSM. Uma plataforma desenvolvida para a Pro4Tech, que otimiza o cadastro de títulos e endereços, além de automatizar relatórios de clientes adimplentes e inadimplentes por datas de pagamento, crédito e vencimento.",
     imageUrl: "img/sistemaGestaoFinanceira.png",
     link: "https://github.com/Equipe-Ace/Ace-documentation"
   },
@@ -19,7 +19,7 @@ export const projects = [
     id: 3,
     title: "Sistema de Datalogger",
     technologies: "React • Python • C • Arduino",
-    description: "Desenvolvimento de uma solução para monitoramento ambiental em tempo real, utilizando Arduino para coleta de dados, C para programação do hardware e Python para processamento e análise de variáveis como temperatura e umidade.",
+    description: "Projeto desenvolvido na API do 4º semestre de DSM. Desenvolvimento de uma solução para monitoramento ambiental em tempo real, utilizando Arduino para coleta de dados, C para programação do hardware e Python para processamento e análise de variáveis como temperatura e umidade.",
     imageUrl: "img/sistemaDataLogger.png",
     link: "https://github.com/Equipe-CodeLand/API-2024.2"
   },
